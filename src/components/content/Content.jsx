bec5017082f71e8be339bae917398867a9eb8b4d
Tiago-ers <trsilva284@gmail.com>
2021-05-06 13:19:32 -0300

    new deploy

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {HashRouter,  Route, Switch } from 'react-router-dom';
 import PageCaracthers from '../pages/PageCaracthers';
 import PageComics from '../pages/PageComics';
 import PageDetails from '../pages/PageDetails';
